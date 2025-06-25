f2625dc2453707cbb34cfba8361ec8e02cdf5eeb
samirpandey-07 <samirjee187@gmail.com>
2025-06-25 17:39:23 +0530

    new

---
+++ practice/recursion_1ton.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+void call(int n){
+    if(n==0){
+        return;
+    }
+    call(n-1);
+    cout<<n<<" ";
+}
+int main(){
+    int n;
+    cin>>n;
+    call(n);
+    return 0;
+}
